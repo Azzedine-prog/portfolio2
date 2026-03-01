b2c7f4dae5f92b1720ee53778cfb23cc56e2c390
azzedine-prog <azzedine.lakhdar.ing@gmail.com>
2026-03-01 17:04:22 +0000

    chore: add .nvmrc to specify Node 20.12.0 for Netlify build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0W9m0EjCEf4gL2rV8pDdrmrBpIbpqtZVS1uAfikAkQs=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"uOWMHXHRDgmoHsdLuqKBPPE8naxNIxSgXQF/qiGxwac=\"}"
